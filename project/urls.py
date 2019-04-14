e338e5f12d568c40b388cc2caae3ca507a029a96
DONGYU <zhangdongyu963@outlook.com>
2019-04-15 01:05:42 +0800

    Fixed small bug

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from mlp import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('mlp/', include('mlp.urls')),
+    path('', views.index, name='index')
 ]
